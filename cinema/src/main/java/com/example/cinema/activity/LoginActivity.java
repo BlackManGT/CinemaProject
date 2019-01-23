13c7dcffb323fce87c35cddb27e4fdf2b9940463
Androidjiarui <1450655366@qq.com>
2019-01-23 10:57:36 +0800

    登录页

--- cinema/src/main/java/com/example/cinema/activity/LoginActivity.java
+++ cinema/src/main/java/com/example/cinema/activity/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.cinema.activity;
 
+import android.content.Intent;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.cinema.R;
 
@@ -13,6 +17,14 @@ public class LoginActivity extends AppCompatActivity implements CustomAdapt {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button button = findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
