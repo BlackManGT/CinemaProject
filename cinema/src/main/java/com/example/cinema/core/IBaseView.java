cba1fd3fa2ac096a8aaef4b9caee8b5ad15e8d0b
Blackpeople1 <493000539@qq.com>
2019-01-23 21:19:39 +0800

    重新开始

--- cinema/src/main/java/com/example/cinema/core/IBaseView.java
+++ cinema/src/main/java/com/example/cinema/core/IBaseView.java
@@ -1,11 +1,13 @@
 package com.example.cinema.core;
 
 import com.example.cinema.bean.CinemaBean;
+import com.example.cinema.bean.MoiveBean;
 import com.example.cinema.bean.Result;
 
 import java.util.List;
 
 import io.reactivex.Observable;
+import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
@@ -13,6 +15,25 @@ import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface IBaseView {
+    //热门影院列表
+    @GET("movieApi/movie/v1/findHotMovieList")
+    Observable<Result<List<MoiveBean>>> Popular(@Header("userId")int userId,
+                                               @Header("sessionId")String sessionId,
+                                               @Query("page") int page,
+                                               @Query("count") int count);
+    //正在上映列表
+    @GET("movieApi/movie/v1/findReleaseMovieList")
+    Observable<Result<List<MoiveBean>>> Being(@Header("userId")int userId,
+                                                @Header("sessionId")String sessionId,
+                                                @Query("page") int page,
+                                                @Query("count") int count);
+    //即将上映列表
+    @GET("movieApi/movie/v1/findComingSoonMovieList")
+    Observable<Result<List<MoiveBean>>> Soon(@Header("userId")int userId,
+                                                @Header("sessionId")String sessionId,
+                                                @Query("page") int page,
+                                                @Query("count") int count);
+
     //推荐影院列表
     @GET("movieApi/cinema/v1/findRecommendCinemas")
     Observable<Result<List<CinemaBean>>> Cinema(@Header("userId")int userId,
@@ -32,5 +53,19 @@ public interface IBaseView {
     @FormUrlEncoded
     Observable<Result> login(@Query("phone")String phone,
                                                @Query("pwd")String pwd);
+
+    @POST("movieApi/user1/registerUser")
+    @FormUrlEncoded
+    Observable<Result> regiest(@Field("nickName") String nickName,
+                               @Field("phone") String phone,
+                               @Field("pwd") String pwd,
+                               @Field("pwd2") String pwd2,
+                               @Field("sex") int sex,
+                               @Field("birthday") String birthday,
+                               @Field("imei") String imei,
+                               @Field("ua") String ua,
+                               @Field("screenSize") String screenSize,
+                               @Field("os") String os,
+                               @Field("email") String email);
 
 }
