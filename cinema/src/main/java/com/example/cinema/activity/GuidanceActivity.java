47984b2c499b277a823670c1283b40b86a47c81a
Androidjiarui <1450655366@qq.com>
2019-02-14 20:30:59 +0800

    支付成功

--- cinema/src/main/java/com/example/cinema/activity/GuidanceActivity.java
+++ cinema/src/main/java/com/example/cinema/activity/GuidanceActivity.java
@@ -1,13 +1,18 @@
 package com.example.cinema.activity;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
 import com.bw.movie.R;
+import com.example.cinema.view.Constant;
 
 
 public class GuidanceActivity extends AppCompatActivity {
@@ -27,6 +32,12 @@ public class GuidanceActivity extends AppCompatActivity {
         sp = getSharedPreferences("config", MODE_PRIVATE);
         boolean boot = sp.getBoolean("boot", true);
         if(boot){
+            if (ContextCompat.checkSelfPermission(GuidanceActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // 申请权限
+                ActivityCompat.requestPermissions(GuidanceActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.ACCESS_NETWORK_STATE}, Constant.REQ_PERM_CAMERA);
+            }
+
             Intent intent = new Intent(GuidanceActivity.this,BootpageActivity.class);
             startActivity(intent);
             finish();
@@ -34,6 +45,12 @@ public class GuidanceActivity extends AppCompatActivity {
             edit.putBoolean("boot",false);
             edit.commit();
         }else{
+            if (ContextCompat.checkSelfPermission(GuidanceActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // 申请权限
+                ActivityCompat.requestPermissions(GuidanceActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_WIFI_STATE, Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, Constant.REQ_PERM_CAMERA);
+            }
+
             Intent intent = new Intent(GuidanceActivity.this,StartupPageActivity.class);
             startActivity(intent);
             finish();
